adbdaf89732eb3ec8f9ab089025316517b50da04
sanjivch <sanjiv.ch09@gmail.com>
2022-11-09 00:15:45 +0530

    Partially complete mid term project

--- src/predict.py
+++ src/predict.py
@@ -1,3 +1,53 @@
-import pandas as pd
-# Loading the model
-# Serving it via a web serice (with Flask or specialized sofware - BentoML, KServe, etc)
+import pickle
+import sklearn
+
+# define model and transformer paths
+model_file = 'model_rf.pkl'
+dv_file = 'dict_vect.pkl'
+
+#Provide the input
+student = {'school': 'MS',
+ 'sex': 'F',
+ 'age': 18,
+ 'address': 'U',
+ 'famsize': 'GT3',
+ 'Pstatus': 'T',
+ 'Medu': 1,
+ 'Fedu': 2,
+ 'Mjob': 'other',
+ 'Fjob': 'other',
+ 'reason': 'course',
+ 'guardian': 'father',
+ 'traveltime': 1,
+ 'studytime': 2,
+ 'failures': 1,
+ 'schoolsup': 'no',
+ 'famsup': 'yes',
+ 'paid': 'no',
+ 'activities': 'yes',
+ 'nursery': 'yes',
+ 'higher': 'yes',
+ 'internet': 'yes',
+ 'romantic': 'yes',
+ 'famrel': 3,
+ 'freetime': 4,
+ 'goout': 4,
+ 'Dalc': 2,
+ 'Walc': 3,
+ 'health': 5,
+ 'absences': 9}
+
+# load the model file
+with open(model_file, 'rb') as f:
+    model = pickle.load(f)
+    
+# load the transformer
+with open(dv_file, 'rb') as f:
+    dict_vect = pickle.load(f)
+
+# Transform the X values
+X = dict_vect.transform(client)
+
+# Predict the instance 
+prediction = model.predict(X)
+print(f"Predicted grade of the student: {prediction}")
--- src/train.py
+++ src/train.py
@@ -1,14 +1,51 @@
 import pandas as pd
 import numpy as np
 
-# Notebook (suggested name - notebook.ipynb) with
-# Data preparation and data clearning
-# EDA, feature importance analysis
-# Model selection process and parameter tuning
-
-# Training the final model
-# Saving it to a file (e.g. pickle) or saving it with specialized software (BentoML)
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+import pickle
 
 # Load data
+df = pd.read_csv('../data/student-por.csv')
 
-# 
+# Split data into train, val nad test datsets 80-20-20 ratio
+df_train_full, df_test = train_test_split(df, test_size=0.1, random_state=1)
+df_train, df_val = train_test_split(df_train_full, test_size=0.11, random_state=1)
+
+df_train = df_train.reset_index(drop=True)
+df_val = df_val.reset_index(drop=True)
+df_test = df_test.reset_index(drop=True)
+
+y_train = df_train["G3"].values
+y_val = df_val["G3"].values
+y_test = df_test["G3"].values
+
+del df_train["G3"]
+del df_val["G3"]
+del df_test["G3"]
+
+# Features to model
+features = ['school', 'sex', 'age', 'address', 'famsize', 'Pstatus', 'Medu', 'Fedu',
+       'Mjob', 'Fjob', 'reason', 'guardian', 'traveltime', 'studytime',
+       'failures', 'schoolsup', 'famsup', 'paid', 'activities', 'nursery',
+       'higher', 'internet', 'romantic', 'famrel', 'freetime', 'goout', 'Dalc',
+       'Walc', 'health', 'absences']
+
+# Transform categorical variables
+train_dict = df_train[features].to_dict(orient='records')
+dict_vect = DictVectorizer(sparse=False)
+
+X_train = dict_vect.fit_transform(train_dict)
+
+# Train the data
+model_rf = RandomForestRegressor()
+model_rf.fit(X_train, y_train)
+
+# save the model as pickle file
+with open('model_rf.pkl', 'wb') as file:
+    pickle.dump(model_rf, file)
+    
+# Save the dict_vect transformer as pickle file
+with open('dict_vect.pkl', 'wb') as file:
+    pickle.dump(dict_vect, file)
